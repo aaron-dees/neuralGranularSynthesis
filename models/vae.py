b9c8c4efa1c6960ac5ce3e4cc7fd9fe01516b5a8
Aaron Dees <deesa@tcd.ie>
2023-02-27 14:14:29 +0000

    Updated architecture for vae

--- models/vae.py
+++ models/vae.py
@@ -14,19 +14,12 @@ import time
 device = torch.device('mps:0')
 # device = torch.device('cpu')
 TRAIN = True
-BATCH_SIZE = 30
-EPOCHS = 20
+BATCH_SIZE = 60
+EPOCHS = 150
+LEARNING_RATE = 0.0005
 SAVE_MODEL = True
 LOAD_MODEL = True
-MODEL_PATH = './saved_models/fsdd_vae_gpu_test.pt'
-
-# MNIST Preprocessing
-transform = tv.transforms.ToTensor()
-trainset = tv.datasets.MNIST(root='./data',  train=True,download=True, transform=transform)
-dataloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, num_workers=0)
-testset = tv.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, num_workers=0)
-
+MODEL_PATH = './saved_models/fsdd_vae_gpu_150epochs.pt'
 
 class FSDDDataset(torch.utils.data.Dataset):
 
@@ -57,7 +50,7 @@ class FSDDDataset(torch.utils.data.Dataset):
 def sample_from_distribution(mu, log_variance):
 
     # point = mu + sigma*sample(N(0,1))
-    epsilon = torch.normal(torch.Tensor([0, 0]).to(device), torch.Tensor([1, 1]).to(device))
+    epsilon = torch.normal(torch.zeros(128).to(device), torch.ones(128).to(device))
     sampled_point = mu + torch.exp(log_variance / 2) * epsilon
 
     return sampled_point
@@ -98,18 +91,20 @@ class Encoder(nn.Module):
 
         # pad = (3 // 2 + (3 - 2 * (3 // 2)) - 1, 3 // 2)
 
-        self.Conv_E_1 = nn.Conv2d(1, 32, stride=1, kernel_size=3, padding=3//2)
-        self.Conv_E_2= nn.Conv2d(32, 64, stride=2, kernel_size=3, padding=3//2)
-        self.Conv_E_3= nn.Conv2d(64, 64, stride=2, kernel_size=3, padding=3//2)
-        self.Conv_E_4= nn.Conv2d(64, 64, stride=1, kernel_size=3, padding=3//2)
-        self.Dense_E_1 = nn.Linear(65536,2)
+        self.Conv_E_1 = nn.Conv2d(1, 512, stride=2, kernel_size=3, padding=3//2)
+        self.Conv_E_2= nn.Conv2d(512, 256, stride=2, kernel_size=3, padding=3//2)
+        self.Conv_E_3= nn.Conv2d(256, 128, stride=2, kernel_size=3, padding=3//2)
+        self.Conv_E_4= nn.Conv2d(128, 64, stride=2, kernel_size=3, padding=3//2)
+        self.Conv_E_5= nn.Conv2d(64, 32, stride=(2,1), kernel_size=3, padding=3//2)
+        self.Dense_E_1 = nn.Linear(1024,128)
 
         self.Flat_E_1 = nn.Flatten()
 
-        self.Norm_E_1 = nn.BatchNorm2d(32)
-        self.Norm_E_2 = nn.BatchNorm2d(64)
-        self.Norm_E_3 = nn.BatchNorm2d(64)
+        self.Norm_E_1 = nn.BatchNorm2d(512)
+        self.Norm_E_2 = nn.BatchNorm2d(256)
+        self.Norm_E_3 = nn.BatchNorm2d(128)
         self.Norm_E_4 = nn.BatchNorm2d(64)
+        self.Norm_E_5 = nn.BatchNorm2d(32)
 
 
         self.Act_E_1 = nn.ReLU()
@@ -135,8 +130,12 @@ class Encoder(nn.Module):
         Conv_E_4 = self.Conv_E_4(Norm_E_3)
         Act_E_4 = self.Act_E_1(Conv_E_4)
         Norm_E_4 = self.Norm_E_4(Act_E_4)
+        # Conv layer 5
+        Conv_E_5 = self.Conv_E_5(Norm_E_4)
+        Act_E_5 = self.Act_E_1(Conv_E_5)
+        Norm_E_5 = self.Norm_E_5(Act_E_5)
         # Dense layer for mu and log variance
-        Flat_E_1 = self.Flat_E_1(Norm_E_4)
+        Flat_E_1 = self.Flat_E_1(Norm_E_5)
         mu = self.Dense_E_1(Flat_E_1)
         log_variance = self.Dense_E_1(Flat_E_1)
 
@@ -151,16 +150,18 @@ class Decoder(nn.Module):
 
         # pad = (3 // 2 + (3 - 2 * (3 // 2)) - 1, 3 // 2)
 
-        self.Dense_D_1 = nn.Linear(2, 65536)
-        self.ConvT_D_1 = nn.ConvTranspose2d(64, 64, stride=1, kernel_size=3, padding = 3//2, output_padding = 0)
+        self.Dense_D_1 = nn.Linear(128, 1024)
+        self.ConvT_D_1 = nn.ConvTranspose2d(32, 32, stride=(2,1), kernel_size=3, padding = 3//2, output_padding = (1,0))
         # Note the need to add output padding here for tranposed dimensions to match
-        self.ConvT_D_2 = nn.ConvTranspose2d(64, 64, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
-        self.ConvT_D_3 = nn.ConvTranspose2d(64, 64, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
-        self.ConvT_D_4 = nn.ConvTranspose2d(64, 1, stride=1, kernel_size=3, padding = 3//2, output_padding = 0)
+        self.ConvT_D_2 = nn.ConvTranspose2d(32, 64, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
+        self.ConvT_D_3 = nn.ConvTranspose2d(64, 128, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
+        self.ConvT_D_4 = nn.ConvTranspose2d(128, 256, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
+        self.ConvT_D_5 = nn.ConvTranspose2d(256, 1, stride=2, kernel_size=3, padding = 3//2, output_padding = 1)
         
-        self.Norm_D_1 = nn.BatchNorm2d(64)
+        self.Norm_D_1 = nn.BatchNorm2d(32)
         self.Norm_D_2 = nn.BatchNorm2d(64)
-        self.Norm_D_3 = nn.BatchNorm2d(64)
+        self.Norm_D_3 = nn.BatchNorm2d(128)
+        self.Norm_D_4 = nn.BatchNorm2d(256)
 
         self.Act_D_1 = nn.ReLU()
         self.Act_D_2 = nn.Sigmoid()
@@ -171,7 +172,7 @@ class Decoder(nn.Module):
 
         # Dense Layer
         Dense_D_1 = self.Dense_D_1(z)
-        Reshape_D_1 = torch.reshape(Dense_D_1, (BATCH_SIZE, 64, 64, 16))
+        Reshape_D_1 = torch.reshape(Dense_D_1, (BATCH_SIZE, 32, 8, 4))
         # Conv layer 1
         Conv_D_1 = self.ConvT_D_1(Reshape_D_1)
         Act_D_1 = self.Act_D_1(Conv_D_1)
@@ -183,10 +184,14 @@ class Decoder(nn.Module):
         # Conv layer 3
         Conv_D_3 = self.ConvT_D_3(Norm_D_2)
         Act_D_3 = self.Act_D_1(Conv_D_3)
-        Norm_D_3 = self.Norm_D_1(Act_D_3)
-        # Conv layer 4 (output)
-        Conv_D_4= self.ConvT_D_4(Norm_D_3)
-        x_hat = self.Act_D_2(Conv_D_4)
+        Norm_D_3 = self.Norm_D_3(Act_D_3)
+        # Conv layer 4
+        Conv_D_4 = self.ConvT_D_4(Norm_D_3)
+        Act_D_4 = self.Act_D_1(Conv_D_4)
+        Norm_D_4 = self.Norm_D_4(Act_D_4)
+        # Conv layer 5 (output)
+        Conv_D_5= self.ConvT_D_5(Norm_D_4)
+        x_hat = self.Act_D_2(Conv_D_5)
 
         return x_hat
 
@@ -253,9 +258,8 @@ if "main":
         # Training
         ########## 
 
-        
         loss_fn = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+        optimizer = torch.optim.Adam(model.parameters(),lr=LEARNING_RATE)
         
         for epoch in range(EPOCHS):
             train_loss = 0.0
@@ -265,13 +269,13 @@ if "main":
                 # no need to flatten images
                 optimizer.zero_grad()                   # clear the gradients
                 x_hat, z, mu, log_variance = model(img)                 # forward pass: compute predicted outputs 
-                loss = calc_combined_loss(x_hat, img, mu, log_variance, 1000)       # calculate the loss
+                loss = calc_combined_loss(x_hat, img, mu, log_variance, 1000000)       # calculate the loss
                 loss.backward()                         # backward pass
                 optimizer.step()                        # perform optimization step
                 train_loss += loss.item()*img.size(0)   # update running training loss
             
             # print avg training statistics 
-            train_loss = train_loss/len(dataloader)
+            train_loss = train_loss/len(fsdd_dataloader)
             print('Epoch: {}'.format(epoch+1),
             '\tTraining Loss: {:.4f}'.format(train_loss))
 
@@ -287,7 +291,7 @@ if "main":
                 model.load_state_dict(torch.load(MODEL_PATH))
 
             # Lets get batch of test images
-            dataiter = iter(testloader)
+            dataiter = iter(fsdd_dataloader)
             images, labels = next(dataiter)
             images = images.to(device)
 
